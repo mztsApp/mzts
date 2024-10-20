fb29aacecd24812b6c1dbf56940d6e4b182c0560
trete tete <gabriel.gladki2003@gmail.com>
2024-10-20 19:04:20 +0200

    feat: Create basic scss helpers and variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Layout } from "@/components/Layout/Layout.server";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <Layout as="div">test dupa cyce</Layout>
+    </div>
+  );
 }
---
+++ src/components/Layout/Layout.constants.ts
@@ -0,0 +1,7 @@
+export const LAYOUT_COMPONENTS = {
+  MAIN: "main",
+  ARTICLE: "article",
+  DIV: "div",
+  HEADER: "header",
+  SECTION: "section",
+} as const;

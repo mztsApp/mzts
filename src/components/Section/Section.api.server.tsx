0114155b420dcc5b449b0625bd16bc09a2207e14
Gabriel <gabriel.gladki2003@gmail.com>
2025-01-02 01:15:53 +0100

    build: Fix api section

--- src/components/Section/Section.api.server.tsx
+++ src/components/Section/Section.api.server.tsx
@@ -1,9 +1,4 @@
 import { sectionDataQuery } from './api/sectionDataQuery';
-import {
-  SECTION_COMPONENT,
-  SECTION_HEADING_COMPONENT,
-} from './Section.constants';
-import { Section } from './Section';
 
 type SectionApiWrapperProps = {
   apiId: string;
@@ -12,11 +7,5 @@ type SectionApiWrapperProps = {
 export const SectionApiWrapper = async ({ apiId }: SectionApiWrapperProps) => {
   const {} = await sectionDataQuery(apiId);
 
-  return (
-    <Section
-      as={SECTION_COMPONENT.SECTION}
-      headingLevel={SECTION_HEADING_COMPONENT.H2}
-      headingText="section"
-    />
-  );
+  return null;
 };

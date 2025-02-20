2c4f92c5dfdc197c778662b95e47359ba623afd0
Gabriel <gabriel.gladki2003@gmail.com>
2025-02-21 00:36:24 +0100

    feat: Add link additional content

--- src/components/AdditionalSectionContent/AdditionalContent.server.tsx
+++ src/components/AdditionalSectionContent/AdditionalContent.server.tsx
@@ -1,9 +1,20 @@
+import { Button } from '../Button/Button';
+import { BUTTON_COMPONENTS } from '../Button/Button.constants';
 import { ADDITIONAL_CONTENT_VARIANT_BY_IDENTIFICATION } from './AdditionalContent.constants';
 import { getAdditionalContent } from './api/getAdditionalContent';
+import styles from './AdditionalSectionContent.module.scss';
 
 type AdditionalSectionContentProps = {
   hash: string;
 };
+
+const getShouldOpenInNewWindowProps = (shouldOpenInNewWindow: boolean) =>
+  shouldOpenInNewWindow
+    ? {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      }
+    : {};
 
 export const AdditionalSectionContent = async ({
   hash,
@@ -18,7 +29,21 @@ export const AdditionalSectionContent = async ({
     case ADDITIONAL_CONTENT_VARIANT_BY_IDENTIFICATION.STYLED_LIST:
       return null;
     case ADDITIONAL_CONTENT_VARIANT_BY_IDENTIFICATION.LINKS:
-      return null;
+      return (
+        <ul className={styles.additionalContentLinks}>
+          {data.items.map(({ text, href, shouldOpenInNewWindow }) => (
+            <li key={text}>
+              <Button
+                {...getShouldOpenInNewWindowProps(shouldOpenInNewWindow)}
+                as={BUTTON_COMPONENTS.ANCHOR}
+                href={href}
+              >
+                {text}
+              </Button>
+            </li>
+          ))}
+        </ul>
+      );
     default:
       return null;
   }
